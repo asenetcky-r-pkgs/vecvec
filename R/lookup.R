aef432539ea78178b2020ea55a581a082f77a5fa
asenetcky <asenetcky@gmail.com>
2025-04-12 16:24:09 -0400

    feat: add lookup prep

---
+++ R/lookup.R
@@ -0,0 +1,7 @@
+prep_lookup <- function(lookup) {
+  original_row_id <- NULL
+
+  prep_stnd_string(lookup) |>
+    dplyr::rename(lookup_id = original_row_id) |>
+    dplyr::select(-"unique_string_id")
+}
