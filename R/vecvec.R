1cf31d39d9abbe8f2db3cfe7c9578eab36ba1e4e
asenetcky <asenetcky@gmail.com>
2025-04-14 11:48:37 -0400

    feat: impl matches to keep

--- R/vecvec.R
+++ R/vecvec.R
@@ -37,43 +37,55 @@ vecvec <- function(primary, lookup) {
 }
 
 create_match_matrix <- function(.vecvec) {
+  value <- lookup_id <- NULL
+
   match_matrix <-
     .vecvec$indices$primary$data_index |>
     dplyr::select(
-      original_row_id,
-      unique_string_id,
+      "original_row_id",
+      "unique_string_id",
       primary = value
     )
 
   # TODO: implement some purrr magic + mini functions
-  ewhlstr <-
+  whole <-
     .vecvec$matches$exact_whole_string |>
-    dplyr::rename(exact_whole_string = lookup_id)
+    dplyr::rename(whole = lookup_id)
 
-  etok <-
+  token <-
     .vecvec$matches$tidy_exact_tokens |>
-    dplyr::rename(exact_token = lookup_id)
+    dplyr::rename(token = lookup_id)
 
-  esubstr <-
+  substring <-
     .vecvec$matches$tidy_exact_substring |>
-    dplyr::rename(exact_substring = lookup_id)
+    dplyr::rename(substring = lookup_id)
 
-  fuzz <-
+  fuzzy <-
     .vecvec$matches$tidy_fuzzies |>
     dplyr::rename(fuzzy = lookup_id)
 
   r <- "many-to-many"
   match_matrix |>
-    dplyr::left_join(ewhlstr, by = "unique_string_id", relationship = r) |>
-    dplyr::left_join(etok, by = "unique_string_id", relationship = r) |>
-    dplyr::left_join(esubstr, by = "unique_string_id", relationship = r) |>
-    dplyr::left_join(fuzz, by = "unique_string_id", relationship = r)
+    dplyr::left_join(whole, by = "unique_string_id", relationship = r) |>
+    dplyr::left_join(token, by = "unique_string_id", relationship = r) |>
+    dplyr::left_join(substring, by = "unique_string_id", relationship = r) |>
+    dplyr::left_join(fuzzy, by = "unique_string_id", relationship = r)
 }
 
-unique_matches <- function(match_matrix) {
+unique_matches <- function(
+  match_matrix,
+  match_vars = c(
+    "whole",
+    "token",
+    "substring",
+    "fuzzy"
+  )
+) {
+  primary <- lookup_id <- original_row_id <- NULL
+
   match_matrix |>
     tidyr::pivot_longer(
-      cols = c(exact_whole_string, exact_token, exact_substring, fuzzy),
+      cols = {{ match_vars }},
       names_to = "match_type",
       values_to = "lookup_id"
     ) |>
@@ -83,17 +95,33 @@ unique_matches <- function(match_matrix) {
     dplyr::filter(!is.na(lookup_id))
 }
 
-human_readable <- function(.vecvec) {
+human_readable <- function(
+  .vecvec,
+  match_vars = c(
+    "whole",
+    "token",
+    "substring",
+    "fuzzy"
+  )
+) {
   lookup <-
     .vecvec$indices$lookup$data_index |>
     dplyr::select(
-      lookup_id = original_row_id,
-      lookup_value = value
+      lookup_id = "original_row_id",
+      lookup_value = "value"
+    )
+
+  perm_vars <-
+    c(
+      "original_row_id",
+      "unique_string_id",
+      "primary"
     )
 
   .vecvec |>
-    unique_matches() |>
     create_match_matrix() |>
+    dplyr::select(dplyr::all_of(c(perm_vars, match_vars))) |>
+    unique_matches(match_vars) |>
     dplyr::left_join(
       lookup,
       by = "lookup_id"
