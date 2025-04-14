fe49e3e44e56dc22ba3277881f2128f715fe6f4f
asenetcky <asenetcky@gmail.com>
2025-04-14 10:09:21 -0400

    feat: implement vecvec parsing

--- R/vecvec.R
+++ R/vecvec.R
@@ -35,3 +35,67 @@ vecvec <- function(primary, lookup) {
     matches = matches
   )
 }
+
+grab_match_matrix <- function(.vecvec) {
+  match_matrix <-
+    .vecvec$indices$primary$data_index |>
+    dplyr::select(
+      original_row_id,
+      unique_string_id,
+      primary = value
+    )
+
+  # TODO: implement some purrr magic + mini functions
+  ewhlstr <-
+    .vecvec$matches$exact_whole_string |>
+    dplyr::rename(exact_whole_string = lookup_id)
+
+  etok <-
+    .vecvec$matches$tidy_exact_tokens |>
+    dplyr::rename(exact_token = lookup_id)
+
+  esubstr <-
+    .vecvec$matches$tidy_exact_substring |>
+    dplyr::rename(exact_substring = lookup_id)
+
+  fuzz <-
+    .vecvec$matches$tidy_fuzzies |>
+    dplyr::rename(fuzzy = lookup_id)
+
+  r <- "many-to-many"
+  match_matrix |>
+    dplyr::left_join(ewhlstr, by = "unique_string_id", relationship = r) |>
+    dplyr::left_join(etok, by = "unique_string_id", relationship = r) |>
+    dplyr::left_join(esubstr, by = "unique_string_id", relationship = r) |>
+    dplyr::left_join(fuzz, by = "unique_string_id", relationship = r)
+}
+
+unique_matches <- function(match_matrix) {
+  match_matrix |>
+    tidyr::pivot_longer(
+      cols = c(exact_whole_string, exact_token, exact_substring, fuzzy),
+      names_to = "match_type",
+      values_to = "lookup_id"
+    ) |>
+    dplyr::group_by(original_row_id, primary) |>
+    dplyr::reframe(lookup_id = unique(lookup_id)) |>
+    dplyr::ungroup() |>
+    dplyr::filter(!is.na(lookup_id))
+}
+
+human_readable <- function(.vecvec) {
+  lookup <-
+    .vecvec$indices$lookup$data_index |>
+    dplyr::select(
+      lookup_id = original_row_id,
+      lookup_value = value
+    )
+
+  .vecvec |>
+    grab_match_matrix() |>
+    unique_matches() |>
+    dplyr::left_join(
+      lookup,
+      by = "lookup_id"
+    )
+}
